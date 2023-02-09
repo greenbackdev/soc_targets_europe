0336899a05d8caf26c837559087a951debaf7c72
lorpacini <lorenza.pacini@greenback.green>
2023-02-09 19:10:43 +0100

    first commit - pedoclimatic clustering

---
+++ evaluate_pedoclimatic_clustering.py
@@ -0,0 +1,289 @@
+import os
+from copy import copy
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from sklearn import mixture
+from sklearn.cluster import AgglomerativeClustering
+import seaborn as sns
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+from models.pedoclimatic_clustering import CarbonContextualizer, get_lucas_data
+
+
+class PedoclimaticClusteringAnalyis:
+
+    def __init__(
+        self,
+        clustering_method_climate,
+        clustering_method_soil,
+        kwargs_clustering_climate,
+        kwargs_clustering_soil,
+        n_clusters,
+        figures_folder,
+    ):
+        self.clustering_method_climate = clustering_method_climate
+        self.clustering_method_soil = clustering_method_soil
+        self.kwargs_clustering_climate = kwargs_clustering_climate
+        self.kwargs_clustering_soil = kwargs_clustering_soil
+        self.n_clusters = n_clusters
+        self.figures_folder = figures_folder
+
+        os.makedirs(figures_folder, exist_ok=True)
+
+    def _run_clustering(self):
+        silhouette_scores_soil = []
+        davies_bouldin_scores_soil = []
+        silhouette_scores_climate = []
+        davies_bouldin_scores_climate = []
+
+        output_clustering = []
+
+        for n in self.n_clusters:
+
+            context = CarbonContextualizer(data=data,
+                                           clustering_method_climate=self.clustering_method_climate,
+                                           clustering_method_soil=self.clustering_method_soil,
+                                           n_clusters_climate=n,
+                                           n_clusters_soil=n,
+                                           kwargs_clustering_climate=self.kwargs_clustering_climate,
+                                           kwargs_clustering_soil=self.kwargs_clustering_soil)
+            context.run()
+            output_clustering.append(copy(context.data))
+            silhouette_scores_soil.append(context.silhouette_score_soil)
+            davies_bouldin_scores_soil.append(
+                context.davies_bouldin_score_soil)
+            silhouette_scores_climate.append(context.silhouette_score_climate)
+            davies_bouldin_scores_climate.append(
+                context.davies_bouldin_score_climate)
+
+        return (
+            output_clustering,
+            silhouette_scores_soil,
+            davies_bouldin_scores_soil,
+            silhouette_scores_climate,
+            davies_bouldin_scores_climate
+        )
+
+    def _plot_metrics(
+            self,
+            silhouette_scores_soil,
+            davies_bouldin_scores_soil,
+            silhouette_scores_climate,
+            davies_bouldin_scores_climate
+    ):
+
+        n_clusters = self.n_clusters
+        figures_folder = self.figures_folder
+
+        fig, axs = plt.subplots(2, 1, figsize=(8, 10))
+        axs[0].plot(n_clusters, silhouette_scores_soil, 'o-')
+        axs[1].plot(n_clusters, davies_bouldin_scores_soil, 'o-')
+        axs[0].set_xticks(n_clusters)
+        axs[0].set_xlabel('n_clusters')
+        axs[0].set_ylabel('Silhouette score')
+        axs[0].set_title('Soil - Silhouette Score')
+        axs[0].grid()
+
+        axs[1].set_xticks(n_clusters)
+        axs[1].set_xlabel('n_clusters')
+        axs[1].set_ylabel('Davies Bouldin score')
+        axs[1].set_title('Soil - Davies Bouldin Score')
+        axs[1].grid()
+        plt.tight_layout()
+        plt.savefig(
+            os.path.join(
+                figures_folder,
+                'clustering_evaluation_soil.pdf'
+            )
+        )
+
+        fig, axs = plt.subplots(2, 1, figsize=(8, 10))
+        axs[0].plot(n_clusters, silhouette_scores_climate, 'o-')
+        axs[1].plot(n_clusters, davies_bouldin_scores_climate, 'o-')
+        axs[0].set_xticks(n_clusters)
+        axs[0].set_xlabel('n_clusters')
+        axs[0].set_ylabel('Silhouette score')
+        axs[0].set_title('Climate - Silhouette Score')
+        axs[0].grid()
+
+        axs[1].set_xticks(n_clusters)
+        axs[1].set_xlabel('n_clusters')
+        axs[1].set_ylabel('Davies Bouldin score')
+        axs[1].set_title('Climate - Davies Bouldin Score')
+        axs[1].grid()
+        plt.tight_layout()
+        plt.savefig(
+            os.path.join(
+                figures_folder,
+                'clustering_evaluation_climate.pdf'
+            )
+        )
+
+        return
+
+    def _plot_clusters_climate_maps(self, output_clustering):
+
+        europe = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+        europe = europe[europe.continent == 'Europe']
+
+        figures_folder = os.path.join(
+            self.figures_folder, 'climate_clusters_maps')
+        os.makedirs(figures_folder, exist_ok=True)
+
+        for df in output_clustering:
+            cluster_labels_climate_ = sorted(df.Cluster_climate.unique())
+            clusters_cmap_climate_ = plt.cm.get_cmap(
+                'tab20', len(cluster_labels_climate_))
+
+            fig, ax = plt.subplots(figsize=(10, 10))
+
+            europe[europe.name != "Russia"].geometry.boundary.plot(
+                ax=ax, linewidth=1, facecolor='gray', alpha=0.1, color='black')
+            ax.set_xlim(-15, 40)
+            ax.set_ylim(32, 75)
+
+            df.plot('Cluster_climate', markersize=3, ax=ax,
+                    legend=True, categorical=True, cmap=clusters_cmap_climate_)
+
+            plt.tight_layout()
+            plt.savefig(
+                os.path.join(
+                    figures_folder,
+                    f'{len(cluster_labels_climate_)}_climate_clusters.pdf'
+                )
+            )
+
+        return
+
+    @staticmethod
+    def _boxplot(x, y, df, palette=None, title=None, ax=None):
+        grouped = df.loc[:, [x, y]].groupby([x]).median().sort_values(by=y)
+        if palette:
+            chart = sns.boxplot(x=x, y=y, data=df,
+                                order=grouped.index, palette=palette, ax=ax)
+        else:
+            chart = sns.boxplot(x=x, y=y, data=df, order=grouped.index, ax=ax)
+        chart.set(title=title)
+        chart.set_xticklabels(chart.get_xticklabels(),
+                              rotation=45, horizontalalignment='right')
+
+    def _plot_clusters_soil_carbonates(self, output_clustering):
+        figures_folder = os.path.join(
+            self.figures_folder, 'soil_clusters_carbonates')
+        os.makedirs(figures_folder, exist_ok=True)
+
+        for df in output_clustering:
+            cluster_labels_soil_ = sorted(df.Cluster_soil.unique())
+            clusters_cmap_soil_ = plt.cm.get_cmap(
+                'gist_rainbow', len(cluster_labels_soil_))
+            cluster_colors_soil_ = {lab: clusters_cmap_soil_(
+                i / len(cluster_labels_soil_)) for i, lab in enumerate(cluster_labels_soil_)}
+
+            fig, ax = plt.subplots(figsize=(5, 5))
+            self._boxplot(x="Cluster_soil", y='CaCO3_2015', df=df,
+                          palette=cluster_colors_soil_, ax=ax)
+            plt.tight_layout()
+            plt.savefig(
+                os.path.join(
+                    figures_folder,
+                    f'{len(cluster_labels_soil_)}_soil_clusters.pdf'
+                )
+            )
+        return
+
+    def _plot_clusters_soil_texture(self, output_clustering):
+        figures_folder = os.path.join(
+            self.figures_folder, 'soil_clusters_texture')
+        os.makedirs(figures_folder, exist_ok=True)
+
+        for df in output_clustering:
+            cluster_labels_soil_ = sorted(df.Cluster_soil.unique())
+            for c in cluster_labels_soil_:
+                fig = px.scatter_ternary(df[df.Cluster_soil == c],
+                                         a="clay_2009", b="sand_2009", c="silt_2009",
+                                         title="Cluster_soil {0:d}".format(c), width=500, height=500)
+                fig.write_image(
+                    os.path.join(
+                        figures_folder,
+                        f'{len(cluster_labels_soil_)}_soil_clusters-cluster_{c}.pdf'
+                    )
+                )
+        return
+
+    def run(self):
+        (
+            output_clustering,
+            silhouette_scores_soil,
+            davies_bouldin_scores_soil,
+            silhouette_scores_climate,
+            davies_bouldin_scores_climate
+        ) = self._run_clustering()
+
+        self._plot_metrics(
+            silhouette_scores_soil,
+            davies_bouldin_scores_soil,
+            silhouette_scores_climate,
+            davies_bouldin_scores_climate
+        )
+
+        self._plot_clusters_climate_maps(output_clustering)
+
+        self._plot_clusters_soil_carbonates(output_clustering)
+
+        self._plot_clusters_soil_texture(output_clustering)
+
+        return (
+            output_clustering,
+            silhouette_scores_soil,
+            davies_bouldin_scores_soil,
+            silhouette_scores_climate,
+            davies_bouldin_scores_climate
+        )
+
+
+if __name__ == "__main__":
+
+    data_folder = os.path.join('data', 'pedoclimatic_clustering')
+    figures_folder = os.path.join(
+        'figures', 'pedoclimatic_clustering_evaluation')
+    os.makedirs(figures_folder, exist_ok=True)
+
+    data = get_lucas_data(data_folder)
+
+    # ----- Agglomerative clustering -----
+    ac_clustering_method_climate = AgglomerativeClustering
+    ac_clustering_method_soil = AgglomerativeClustering
+    ac_kwargs_clustering_climate = {"linkage": "ward"}
+    ac_kwargs_clustering_soil = {"linkage": "ward"}
+    # ac_n_clusters = range(3, 20)
+    ac_n_clusters = range(3, 6)
+    ac_figures_folder = os.path.join(
+        figures_folder, 'agglomerative_clustering')
+    _ = PedoclimaticClusteringAnalyis(
+        ac_clustering_method_climate,
+        ac_clustering_method_soil,
+        ac_kwargs_clustering_climate,
+        ac_kwargs_clustering_soil,
+        ac_n_clusters,
+        ac_figures_folder
+    ).run()
+
+    # ----- Gaussian mixture -----
+    gm_clustering_method_climate = mixture.GaussianMixture
+    gm_clustering_method_soil = mixture.GaussianMixture
+    gm_kwargs_clustering_climate = {"covariance_type": "full"}
+    gm_kwargs_clustering_soil = {"covariance_type": "full"}
+    # gm_n_clusters = range(3, 20)
+    gm_n_clusters = range(3, 6)
+    gm_figures_folder = os.path.join(figures_folder, 'gaussian_mixture')
+    _ = PedoclimaticClusteringAnalyis(
+        gm_clustering_method_climate,
+        gm_clustering_method_soil,
+        gm_kwargs_clustering_climate,
+        gm_kwargs_clustering_soil,
+        gm_n_clusters,
+        gm_figures_folder
+    ).run()
---
+++ models/pedoclimatic_clustering.py
@@ -0,0 +1,172 @@
+import os
+
+import pandas as pd
+import geopandas as gpd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import AgglomerativeClustering
+from sklearn import mixture
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import silhouette_score, davies_bouldin_score
+
+
+def get_lucas_data(data_folder, file_name='selected_lucas_data.csv'):
+    data = pd.read_csv(os.path.join(data_folder, file_name))
+    data['geometry'] = gpd.GeoSeries.from_wkt(data['geometry'])
+    data = gpd.GeoDataFrame(data, geometry='geometry')
+    data = data.set_crs("EPSG:4326")
+    data["lat"] = data.geometry.y
+    data["lon"] = data.geometry.x
+    return data
+
+
+class CarbonContextualizer:
+
+    def __init__(
+        self,
+        data,
+        clustering_method_climate=mixture.GaussianMixture,
+        clustering_method_soil=AgglomerativeClustering,
+        n_clusters_climate=11,
+        n_clusters_soil=4,
+        kwargs_clustering_climate={"covariance_type": "full"},
+        kwargs_clustering_soil={"linkage": "ward"},
+    ):
+        """
+        :param str data_path: path to the data folder
+        :param class clustering_method_climate: clustering class for climate. In the current implementation, it assumes that it has a .predict() method. Default is mixture.GaussianMixture from sklearn.
+        :param class clustering_method_climate: clustering class for soil. In the current implementation, it assumes that it dos not have a .predict() method. Default is AgglomerativeClustering from sklearn.cluster.
+        :param int n_clusters_climate: number of climate clusters. Default is 11.
+        :param int n_clusters_soil: number of soil clusters. Default is 4.
+        :param dict kwargs_clustering_climate: arguments to initialize climate clustering instance. Default is {'covariance_type': 'full'}.
+        :param dict kwargs_clustering_soil: arguments to initialize soil clustering instance. Default is {'linkage': 'ward'}.
+
+        """
+        self.data = data
+        self.features_climate = [
+            "tmp_mean",
+            "tmp_std",
+            "pre_sum",
+            "aridity_sum",
+        ]
+        self.features_soil = ["clay_2009", "sand_2009", "CaCO3_2015"]
+        self.clustering_method_climate = clustering_method_climate
+        self.clustering_method_soil = clustering_method_soil
+        self.n_clusters_climate = n_clusters_climate
+        self.n_clusters_soil = n_clusters_soil
+        self.kwargs_clustering_climate = kwargs_clustering_climate
+        self.kwargs_clustering_soil = kwargs_clustering_soil
+
+    def scale_data(self):
+        scaler_soil = StandardScaler()
+        scaler_climate = StandardScaler()
+        data = self.data
+        scaler_climate = scaler_climate.fit(
+            data[self.features_climate].values
+        )
+        scaler_soil = scaler_soil.fit(
+            data[self.features_soil].values
+        )
+
+        X_climate = scaler_climate.transform(
+            data[self.features_climate].values
+        )
+        X_soil = scaler_soil.transform(
+            data[self.features_soil].values
+        )
+        return scaler_climate, scaler_soil, X_climate, X_soil
+
+    @staticmethod
+    def _train_clustering_model(X, method, n_components, kwargs):
+        try:
+            model = method(n_components=n_components, random_state=1, **kwargs)
+        except TypeError:
+            model = method(
+                n_clusters=n_components, **kwargs
+            )  # for Agglomerative Clustering
+        model = model.fit(X)
+        return model
+
+    def train_clustering(self):
+
+        model_climate = self._train_clustering_model(
+            X=self.X_climate,
+            method=self.clustering_method_climate,
+            n_components=self.n_clusters_climate,
+            kwargs=self.kwargs_clustering_climate,
+        )
+        model_soil = self._train_clustering_model(
+            X=self.X_soil,
+            method=self.clustering_method_soil,
+            n_components=self.n_clusters_soil,
+            kwargs=self.kwargs_clustering_soil,
+        )
+
+        # Train a KNN model then predicts the soil clusters of new data
+        # (necessary because Agglomerative Clustering does not have a .predict() method)
+        if not hasattr(model_soil, "predict"):
+            clusters_soil = model_soil.labels_
+            knn_soil = KNeighborsClassifier(n_neighbors=1)
+            knn_soil.fit(self.X_soil, clusters_soil)
+
+        else:
+            knn_soil = None
+
+        self.knn_soil = knn_soil
+
+        return model_climate, model_soil, knn_soil
+
+    def apply_clustering(self):
+
+        data = self.data
+        if not hasattr(self.model_climate, "predict"):
+            clusters_climate = self.model_climate.labels_
+            data["Cluster_climate"] = clusters_climate
+        else:
+            clusters_climate = self.model_climate.predict(self.X_climate)
+            data["Cluster_climate"] = clusters_climate
+
+        if not hasattr(self.model_soil, "predict"):
+            clusters_soil = self.model_soil.labels_
+            data["Cluster_soil"] = clusters_soil
+        else:
+            clusters_soil = self.model_soil.predict(self.X_soil)
+            data["Cluster_soil"] = clusters_soil
+
+        return data
+
+    def evaluate_clustering(self):
+        if hasattr(self.model_climate, "predict"):  # GaussianMixture
+            bic_score_climate = self.model_climate.bic(self.X_climate)
+            silhouette_score_climate = None
+            davies_bouldin_score_climate = None
+        else:  # Agglomerative Clustering
+            silhouette_score_climate = silhouette_score(
+                self.X_climate, self.model_climate.labels_, metric='euclidean')
+            davies_bouldin_score_climate = davies_bouldin_score(
+                self.X_climate, self.model_climate.labels_)
+            bic_score_climate = None
+
+        if hasattr(self.model_soil, "predict"):  # GaussianMixture
+            bic_score_soil = self.model_soil.bic(self.X_soil)
+            silhouette_score_soil = None
+            davies_bouldin_score_soil = None
+        else:  # Agglomerative Clustering
+            silhouette_score_soil = silhouette_score(
+                self.X_soil, self.model_soil.labels_, metric='euclidean')
+            davies_bouldin_score_soil = davies_bouldin_score(
+                self.X_soil, self.model_soil.labels_)
+            bic_score_soil = None
+
+        return silhouette_score_climate, davies_bouldin_score_climate, bic_score_climate, silhouette_score_soil, davies_bouldin_score_soil, bic_score_soil
+
+    def run(self):
+
+        self.scaler_climate, self.scaler_soil, self.X_climate, self.X_soil = self.scale_data()
+        self.model_climate, self.model_soil, self.knn_soil = self.train_clustering()
+        self.data = self.apply_clustering()
+        (self.silhouette_score_climate,
+         self.davies_bouldin_score_climate,
+         self.bic_score_climate,
+         self.silhouette_score_soil,
+         self.davies_bouldin_score_soil,
+         self.bic_score_soil) = self.evaluate_clustering()
